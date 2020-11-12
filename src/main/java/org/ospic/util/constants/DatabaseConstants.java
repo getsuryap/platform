a826b5b41ecb6ab279bec9891aadc379e8098f58
=elirehema <=rocksandystone@gmail.com>
2020-11-12 17:41:25 +0300

    Modify Diagnosis reports

--- src/main/java/org/ospic/util/constants/DatabaseConstants.java
+++ src/main/java/org/ospic/util/constants/DatabaseConstants.java
@@ -28,7 +28,7 @@ public class DatabaseConstants {
     public static final String WARDS_TABLE = "m_wards";
     public static final String PHYSICIAN_TABLE = "m_physician";
     public static final String TABLE_ADMISSION_INFO = "m_admissions";
-    public static final String DIAGNOSES_TABLE = "m_diagnosis";
+    public static final String DIAGNOSES_TABLE = "m_diagnoses";
     public static final String PRIVILEGE_TABLE = "m_privilege";
 
     public static final String GENDER_TABLE = "m_gender";
