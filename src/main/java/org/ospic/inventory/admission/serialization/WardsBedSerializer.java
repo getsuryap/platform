8d443ff37fc7cb7ce95a9ec50ce123c75687e127
=elirehema <=rocksandystone@gmail.com>
2020-11-21 20:49:35 +0300

    serialization of objects

--- src/main/java/org/ospic/inventory/admission/serialization/WardsBedSerializer.java
+++ src/main/java/org/ospic/inventory/admission/serialization/WardsBedSerializer.java
@@ -1,4 +1,4 @@
-package org.ospic.inventory.admission;
+package org.ospic.inventory.admission.serialization;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
