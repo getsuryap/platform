38d77af96254b90bb706558bb491e4e61001e8e8
=elirehema <=rocksandystone@gmail.com>
2021-01-09 11:05:22 +0300

    Department classes

---
+++ src/main/java/org/ospic/platform/organization/departments/api/DepartmentApiResource.java
@@ -0,0 +1,38 @@
+package org.ospic.platform.organization.departments.api;
+
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * This file was created by eli on 09/01/2021 for org.ospic.platform.organization.departments.api
+ * --
+ * --
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/departments")
+@Api(value = "/api/departments", tags = "Department Api's")
+public class DepartmentApiResource {
+
+    @Autowired
+    public DepartmentApiResource(){}
+}
