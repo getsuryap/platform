2329910ed8fa96b64712c8107405c1b327fb9ea1
=elirehema <=rocksandystone@gmail.com>
2021-01-25 18:23:44 +0300

    Reports support and management

--- src/main/java/org/ospic/platform/controllers/TestController.java
+++ src/main/java/org/ospic/platform/controllers/TestController.java
@@ -1,17 +1,43 @@
 package org.ospic.platform.controllers;
 
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.ospic.platform.patient.infos.domain.Patient;
+import org.ospic.platform.patient.infos.repository.PatientRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @ApiIgnore
 @RequestMapping("/api/test")
 public class TestController {
+	final ModelAndView model = new ModelAndView();
+	final Logger log = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	ApplicationContext context;
+	@Autowired
+	PatientRepository repository;
+
 	@GetMapping("/all")
 	public String allAccess() {
 		return "Public Content.";
@@ -35,8 +61,47 @@ public class TestController {
 		return "Admin Board.";
 	}
 
-	@GetMapping("/")
-	public String home(){
-		return "home";
+	@GetMapping(value= "/welcome")
+	public ModelAndView index() {
+		log.info("Showing the welcome page.");
+		model.setViewName("welcome");
+		return model;
+	}
+
+	// Method to create the pdf report via jasper framework.
+
+	@GetMapping("/view")
+	public void viewReport(HttpServletResponse response) throws IOException, JRException, SQLException {
+		//response.setContentType("application/x-download");
+		response.setContentType("application/pdf");
+		response.setHeader("Content-Disposition", "inline; filename=" + "example.pdf");
+		//response.setHeader("Content-Disposition", String.format("attachment: filename=\"patients.pdf\""));
+		OutputStream out = response.getOutputStream();
+		this.exportPdfReport(repository.findAll(), out);
+	}
+
+	// Method to create the pdf file using the employee list datasource.
+	private void  exportPdfReport(final List<Patient> employees, OutputStream outputStream) throws JRException, IOException {
+
+		// Fetching the .jrxml file from the resources folder.
+		Resource resource = context.getResource("classpath:jasperreports/patient_list.jrxml");
+		final InputStream stream = resource.getInputStream();
+
+		// Compile the Jasper report from .jrxml to .japser
+		final JasperReport report = JasperCompileManager.compileReport(stream);
+
+		// Fetching the employees from the data source.
+		final JRBeanCollectionDataSource source = new JRBeanCollectionDataSource(employees);
+
+
+		// Adding the additional parameters to the pdf.
+		final Map<String, Object> parameters = new HashMap<>();
+		parameters.put("createdBy", "javacodegeek.com");
+
+		// Filling the report with the employee data and additional parameters information.
+		final JasperPrint print = JasperFillManager.fillReport(report, parameters, source);
+		final String filePath = "\\";
+		// Export the report to a PDF file.
+		 JasperExportManager.exportReportToPdfStream(print, outputStream);
 	}
 }
