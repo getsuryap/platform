1af06060946dfb52bf4be1af19ff8bbf408384dd
=elirehema <=rocksandystone@gmail.com>
2021-02-02 21:26:08 +0300

    Spring boot build war

--- src/main/java/org/ospic/platform/ServletInitializer.java
+++ src/main/java/org/ospic/platform/ServletInitializer.java
@@ -2,7 +2,9 @@ package org.ospic.platform;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ServletInitializer extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
