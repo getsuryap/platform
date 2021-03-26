a29268eea730f93e3492d5c7ee7c6a8ef1f67512
Elirehema <rocksandystone@gmail.com>
2021-03-27 02:07:53 +0300

    Update statistics add monthly and weekly summation

--- src/main/java/org/ospic/platform/accounting/statistics/rowmap/TransactionSummationsRowMap.java
+++ src/main/java/org/ospic/platform/accounting/statistics/rowmap/TransactionSummationsRowMap.java
@@ -32,7 +32,11 @@ public class TransactionSummationsRowMap implements RowMapper<TransactionSummati
         return "  select count(*) as totalNumberOfTransactions, " +
                 "   sum(case when date(tx.transaction_date) =curdate() then 1 else 0 end ) as totalNumberOfTransactionsToday, " +
                 "   sum(tx.amount) as totalTransactionAmount, " +
-                "   sum(case when date(tx.transaction_date) = curdate()  then tx.amount else 0 end ) as totalTransactionAmountToday " +
+                "   sum(case when date(tx.transaction_date) = curdate()  then tx.amount else 0 end ) as totalTransactionAmountToday, " +
+                "   sum(case when date(tx.transaction_date) >= curdate() - interval 6 day  then 1 else 0 end ) as totalNumberOfTransactionsLast7Days,"+
+                "   sum(case when date(tx.transaction_date) >= curdate() - interval 6 day  then tx.amount else 0 end ) as totalTransactionAmountLast7Days, " +
+                "   sum(case when date(tx.transaction_date) >= curdate() - interval 29 day  then 1 else 0 end ) as totalNumberOfTransactionsLast30Days, " +
+                "   sum(case when date(tx.transaction_date) >= curdate() - interval 29 day  then tx.amount else 0 end ) as totalTransactionAmountLast30Days"+
                 "   from m_transactions tx ;";
     }
     @Override
