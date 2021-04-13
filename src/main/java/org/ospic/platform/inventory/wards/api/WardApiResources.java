b732adfd212035e117869f5519cc0589adb3c314
Elirehema <rocksandystone@gmail.com>
2021-04-14 02:26:58 +0300

    delete uneccessary responsebody in wards api

--- src/main/java/org/ospic/platform/inventory/wards/api/WardApiResources.java
+++ src/main/java/org/ospic/platform/inventory/wards/api/WardApiResources.java
@@ -63,14 +63,14 @@ public class WardApiResources {
 
     @ApiOperation(value = "RETRIEVE Wards", notes = "RETRIEVE Wards",response = Ward.class, responseContainer = "List")
     @RequestMapping(value = "/", method = RequestMethod.GET, consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
+    
     ResponseEntity<List<Ward>> retrieveAllWards() {
         return wardReadPrincipleService.retrieveListOfWards();
     }
 
     @ApiOperation(value = "RETRIEVE Wards with beds count", notes = "RETRIEVE Wards with beds count",response = WardResponseData.class, responseContainer = "List")
     @RequestMapping(value = "/beds", method = RequestMethod.GET, consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
+    
     ResponseEntity<List<WardResponseData>> retrieveAllWardsWithBedsCounts() {
         return wardReadPrincipleService.retrieveAllWardsWithBedsCounts();
     }
@@ -78,7 +78,7 @@ public class WardApiResources {
 
     @ApiOperation(value = "RETRIEVE ward by ID", notes = "RETRIEVE ward by ID",response = Ward.class)
     @RequestMapping(value = "/{wardId}", method = RequestMethod.GET, consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
+    
     ResponseEntity<?> retrieveWardById(@PathVariable(value = "wardId", required = true) Long wardId) {
         return wardReadPrincipleService.findById(wardId);
     }
@@ -86,28 +86,28 @@ public class WardApiResources {
 
     @ApiOperation(value = "CREATE new Ward", notes = "CREATE new Ward",response = Ward.class)
     @RequestMapping(value = "/", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.ALL_VALUE)
-    @ResponseBody
+    
     ResponseEntity<?> createNewWard(@Valid @RequestBody Ward ward) {
         return wardWritePrincipleService.createNewWard(ward);
     }
 
     @ApiOperation(value = "UPDATE Ward", notes = "UPDATE Ward",response = Ward.class)
     @RequestMapping(value = "/{wardId}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
+    
     ResponseEntity<?> createNewWard(@PathVariable(value = "wardId", required = true) Long wardId, @Valid @RequestBody Ward ward) {
         return wardWritePrincipleService.updateWard(wardId, ward);
     }
 
     @ApiOperation(value = "DELETE Ward", notes = "DELETE Ward",response = MessageResponse.class)
     @RequestMapping(value = "/{wardId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
+    
     ResponseEntity<?> deleteWard(@PathVariable(value = "wardId", required = true) Long wardId) {
         return wardWritePrincipleService.deleteWard(wardId);
     }
 
     @ApiOperation(value = "CREATE Wards by list array", notes = "CREATE Wards by array",response = Ward.class, responseContainer = "List")
     @RequestMapping(value = "/list", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
+    
     ResponseEntity<String> createWardsByList(@Valid @RequestBody List<Ward> wards) {
         wards.forEach(ward -> {
             if (!wardRepository.existsByName(ward.getName())) {
@@ -119,14 +119,14 @@ public class WardApiResources {
 
     @ApiOperation(value = "ADD new bed in Ward", notes = "ADD new bed in Ward")
     @RequestMapping(value = "/{wardId}/bed", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.ALL_VALUE)
-    @ResponseBody
+    
     ResponseEntity<?> addNewBedInWard(@PathVariable(value = "wardId", required = true) Long wardId, @RequestBody @Valid Bed bed) throws AbstractPlatformInactiveResourceException.ResourceNotFoundException {
         return wardWritePrincipleService.addBedInWard(wardId, bed);
     }
 
     @ApiOperation(value = "ADD new bed in Ward", notes = "ADD new bed in Ward")
     @RequestMapping(value = "/{wardId}/beds", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.ALL_VALUE)
-    @ResponseBody
+    
     ResponseEntity<?> addNewListOfBedsInWard(@PathVariable(value = "wardId", required = true) Long wardId, @RequestBody @Valid List<Bed> beds) throws AbstractPlatformInactiveResourceException.ResourceNotFoundException {
         return wardWritePrincipleService.addListOfBedsInWard(wardId, beds);
     }
