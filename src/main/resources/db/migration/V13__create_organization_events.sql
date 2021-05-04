2d5743b2fd40005cc0e1bfa38f6670d3bc6c6a5c
Elirehema <rocksandystone@gmail.com>
2021-05-04 19:24:36 +0300

    remove default

--- src/main/resources/db/migration/V13__create_organization_events.sql
+++ src/main/resources/db/migration/V13__create_organization_events.sql
@@ -11,7 +11,7 @@ CREATE TABLE `m_calendar` (
   `department` int(11) DEFAULT '0',
   `created_date` varchar(100) DEFAULT NULL,
   `created_by` varchar(200) DEFAULT NULL,
-  `last_modified_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `last_modified_date` timestamp NOT NULL,
   `last_modified_by` varchar(200) DEFAULT NULL,
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
